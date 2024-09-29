b5acc648e9bf57345e7f8904a2c60346a88dd3a9
Tommy Tran <tommytran@motorist.vn>
2024-09-29 14:31:41 +0700

    [hot fix] - fix date input error

--- components/DateInput/index.tsx
+++ components/DateInput/index.tsx
@@ -1,11 +1,11 @@
-import React, { ForwardedRef, HTMLProps } from 'react'
-import { Flex, Input, chakra } from '@chakra-ui/react'
-import Icon from 'components/Icon'
-import dayjs from 'dayjs'
-import advancedFormat from 'dayjs/plugin/advancedFormat'
-import NumberFormat from 'react-number-format'
+import React, { ForwardedRef } from 'react';
+import { Flex, chakra, Input as ChakraInput } from '@chakra-ui/react';
+import Icon from 'components/Icon';
+import dayjs from 'dayjs';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
+import { NumericFormat, NumericFormatProps } from 'react-number-format';
 
-dayjs.extend(advancedFormat)
+dayjs.extend(advancedFormat);
 
 const DatePickerWrapper = chakra(Flex, {
   baseStyle: () => ({
@@ -16,26 +16,32 @@ const DatePickerWrapper = chakra(Flex, {
     paddingBottom: '10px',
     border: 'solid 1px #E2E8F0',
     justifyContent: 'space-between',
-  })
-})
+  }),
+});
 
-export const DateInput = (props: HTMLProps<HTMLInputElement>, ref: ForwardedRef<HTMLInputElement>) => {
-  return (
-    <DatePickerWrapper onClick={props?.onClick} ref={ref}>
-      <Input
-        ref={ref}
-        as={NumberFormat}
-        {...(props as HTMLProps<HTMLInputElement>)}
-        variant="unstyled"
-        size="sm"
-        fontSize="md"
-        format="##/##/####"
-        placeholder="MM/DD/YYYY"
-        mask={['M', 'M', 'D', 'D', 'Y', 'Y', 'Y', 'Y']}
-      />
-      <Icon iconName="date.svg" size={20} />
-    </DatePickerWrapper>
-  )
+interface DateInputProps extends NumericFormatProps {
+  onClick?: () => void;
 }
 
-export default DateInput
+export const DateInput = React.forwardRef<HTMLInputElement, DateInputProps>(
+  (props, ref: ForwardedRef<HTMLInputElement>) => {
+    return (
+      <DatePickerWrapper onClick={props?.onClick}>
+        <NumericFormat
+          {...props}
+          getInputRef={ref}
+          customInput={ChakraInput}
+          format="##/##/####"
+          placeholder="MM/DD/YYYY"
+          mask={['M', 'M', 'D', 'D', 'Y', 'Y', 'Y', 'Y']}
+          variant="unstyled"
+          size="sm"
+          fontSize="md"
+        />
+        <Icon iconName="date.svg" size={20} />
+      </DatePickerWrapper>
+    );
+  }
+);
+
+export default DateInput;
